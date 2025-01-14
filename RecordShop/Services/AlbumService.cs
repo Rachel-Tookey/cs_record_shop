b5e42891827bb23b8a5dfebf5a763cc6cf6632f2
Tookey <rachel.tookey@gmail.com>
2025-01-14 11:21:02 +0000

    Built service layer for album and artist repo

---
+++ RecordShop/Services/AlbumService.cs
@@ -0,0 +1,40 @@
+﻿using RecordShop.Data;
+using RecordShop.Entities;
+using RecordShop.Repository;
+
+namespace RecordShop.Services
+{
+
+    public interface IAlbumService
+    {
+        public List<Album> GetAllAlbums();
+
+        public void AddAlbum(Album album);
+
+    }
+
+
+
+    public class AlbumService : IAlbumService
+    {
+        private readonly IAlbumRepository _albumRepository;
+
+        public AlbumService(IAlbumRepository albumRepository)
+        {
+            _albumRepository = albumRepository;
+
+        }
+
+        public List<Album> GetAllAlbums()
+        {
+            return _albumRepository.GetAllAlbums();
+        }
+
+        public void AddAlbum(Album album)
+        {
+            _albumRepository.AddAlbum(album); 
+        }
+
+
+    }
+}
