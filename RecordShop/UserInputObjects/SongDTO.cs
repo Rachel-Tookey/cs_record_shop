96af42dac2f48a53fab673f99f8ae4fbc3f9ae9f
Tookey <rachel.tookey@gmail.com>
2025-02-02 23:32:42 +0000

    Chore: added json property names to song dto

--- RecordShop/UserInputObjects/SongDTO.cs
+++ RecordShop/UserInputObjects/SongDTO.cs
@@ -1,21 +1,28 @@
 ﻿using RecordShop.Entities;
 using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
 
 namespace RecordShop.UserInputObjects
 {
     public class SongDTO
     {
-
+        
         [Required]
+        [JsonPropertyName("name")]
         public string Name { get; set; }
 
+        [Required]
+        [JsonPropertyName("artistid")]
         public int ArtistId { get; set; }
 
+        [JsonPropertyName("description")]
         public string Description { get; set; }
 
+        [JsonPropertyName("spotifyurl")]
         public string SpotifyUrl { get; set; }
         
         [Required]
+        [JsonPropertyName("releasedate")]
         public DateTime ReleaseDate { get; set; }
 
     }
